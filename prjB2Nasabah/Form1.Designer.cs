02eb848a981915c61d02cb39eb2e235547729833
fikrihelmi17 <fikrihelmi17@gmail.com>
2020-07-03 13:42:54 +0700

    Initial commit

---
+++ prjB2Nasabah/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace prjB2Nasabah
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtNIK = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtTempat = new System.Windows.Forms.TextBox();
+            this.txtAlamat = new System.Windows.Forms.TextBox();
+            this.txtTelepon = new System.Windows.Forms.TextBox();
+            this.rbLaki = new System.Windows.Forms.RadioButton();
+            this.rbPerempuan = new System.Windows.Forms.RadioButton();
+            this.dtpTanggal = new System.Windows.Forms.DateTimePicker();
+            this.cmbPekerjaan = new System.Windows.Forms.ComboBox();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.rtbView = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NIK";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama Lengkap";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tempat, Tanggal Lahir";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Jenis Kelamin";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Alamat";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Pekerjaan";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 200);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "No Telp";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 253);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(96, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Status Perkawinan";
+            // 
+            // txtNIK
+            // 
+            this.txtNIK.Location = new System.Drawing.Point(142, 12);
+            this.txtNIK.Name = "txtNIK";
+            this.txtNIK.Size = new System.Drawing.Size(100, 20);
+            this.txtNIK.TabIndex = 8;
+            this.txtNIK.TextChanged += new System.EventHandler(this.txtNIK_TextChanged);
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(142, 39);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(100, 20);
+            this.txtNama.TabIndex = 9;
+            // 
+            // txtTempat
+            // 
+            this.txtTempat.Location = new System.Drawing.Point(142, 65);
+            this.txtTempat.Name = "txtTempat";
+            this.txtTempat.Size = new System.Drawing.Size(100, 20);
+            this.txtTempat.TabIndex = 10;
+            // 
+            // txtAlamat
+            // 
+            this.txtAlamat.Location = new System.Drawing.Point(142, 129);
+            this.txtAlamat.Multiline = true;
+            this.txtAlamat.Name = "txtAlamat";
+            this.txtAlamat.Size = new System.Drawing.Size(201, 46);
+            this.txtAlamat.TabIndex = 11;
+            // 
+            // txtTelepon
+            // 
+            this.txtTelepon.Location = new System.Drawing.Point(142, 189);
+            this.txtTelepon.Name = "txtTelepon";
+            this.txtTelepon.Size = new System.Drawing.Size(100, 20);
+            this.txtTelepon.TabIndex = 12;
+            this.txtTelepon.TextChanged += new System.EventHandler(this.txtTelepon_TextChanged);
+            // 
+            // rbLaki
+            // 
+            this.rbLaki.AutoSize = true;
+            this.rbLaki.Location = new System.Drawing.Point(142, 102);
+            this.rbLaki.Name = "rbLaki";
+            this.rbLaki.Size = new System.Drawing.Size(68, 17);
+            this.rbLaki.TabIndex = 14;
+            this.rbLaki.TabStop = true;
+            this.rbLaki.Text = "Laki Laki";
+            this.rbLaki.UseVisualStyleBackColor = true;
+            this.rbLaki.CheckedChanged += new System.EventHandler(this.rbLaki_CheckedChanged);
+            // 
+            // rbPerempuan
+            // 
+            this.rbPerempuan.AutoSize = true;
+            this.rbPerempuan.Location = new System.Drawing.Point(249, 100);
+            this.rbPerempuan.Name = "rbPerempuan";
+            this.rbPerempuan.Size = new System.Drawing.Size(79, 17);
+            this.rbPerempuan.TabIndex = 15;
+            this.rbPerempuan.TabStop = true;
+            this.rbPerempuan.Text = "Perempuan";
+            this.rbPerempuan.UseVisualStyleBackColor = true;
+            this.rbPerempuan.CheckedChanged += new System.EventHandler(this.rbPerempuan_CheckedChanged);
+            // 
+            // dtpTanggal
+            // 
+            this.dtpTanggal.CustomFormat = "dd - MMM - yyyy";
+            this.dtpTanggal.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpTanggal.Location = new System.Drawing.Point(249, 65);
+            this.dtpTanggal.Name = "dtpTanggal";
+            this.dtpTanggal.Size = new System.Drawing.Size(94, 20);
+            this.dtpTanggal.TabIndex = 16;
+            this.dtpTanggal.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // cmbPekerjaan
+            // 
+            this.cmbPekerjaan.FormattingEnabled = true;
+            this.cmbPekerjaan.Items.AddRange(new object[] {
+            "Pegawai Negeri Sipil",
+            "Karyawan Swasta",
+            "Wirausaha",
+            "Pelajar"});
+            this.cmbPekerjaan.Location = new System.Drawing.Point(142, 218);
+            this.cmbPekerjaan.Name = "cmbPekerjaan";
+            this.cmbPekerjaan.Size = new System.Drawing.Size(100, 21);
+            this.cmbPekerjaan.TabIndex = 17;
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Location = new System.Drawing.Point(142, 245);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(100, 21);
+            this.cmbStatus.TabIndex = 18;
+            this.cmbStatus.SelectedIndexChanged += new System.EventHandler(this.cmbStatus_SelectedIndexChanged);
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(142, 289);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(41, 23);
+            this.btnView.TabIndex = 19;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(200, 289);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(42, 23);
+            this.btnClear.TabIndex = 20;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // rtbView
+            // 
+            this.rtbView.Location = new System.Drawing.Point(16, 332);
+            this.rtbView.Name = "rtbView";
+            this.rtbView.Size = new System.Drawing.Size(327, 162);
+            this.rtbView.TabIndex = 21;
+            this.rtbView.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 521);
+            this.Controls.Add(this.rtbView);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.cmbPekerjaan);
+            this.Controls.Add(this.dtpTanggal);
+            this.Controls.Add(this.rbPerempuan);
+            this.Controls.Add(this.rbLaki);
+            this.Controls.Add(this.txtTelepon);
+            this.Controls.Add(this.txtAlamat);
+            this.Controls.Add(this.txtTempat);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtNIK);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Nasabah";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtNIK;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtTempat;
+        private System.Windows.Forms.TextBox txtAlamat;
+        private System.Windows.Forms.TextBox txtTelepon;
+        private System.Windows.Forms.RadioButton rbLaki;
+        private System.Windows.Forms.RadioButton rbPerempuan;
+        private System.Windows.Forms.DateTimePicker dtpTanggal;
+        private System.Windows.Forms.ComboBox cmbPekerjaan;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.RichTextBox rtbView;
+    }
+}
+
---
+++ prjB2Nasabah/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjB2Nasabah
+{
+    public partial class Form1 : Form
+    {
+        string JenisKelamin;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbStatus.Items.Add("Lajang");
+            cmbStatus.Items.Add("Menikah");
+            cmbStatus.Items.Add("Duda");
+            cmbStatus.Items.Add("Janda");
+        }
+
+        private void rbLaki_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbLaki.Checked)
+            {
+                JenisKelamin = rbLaki.Text;
+            }
+        }
+
+        private void rbPerempuan_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbPerempuan.Checked)
+            {
+                JenisKelamin = rbPerempuan.Text;
+            }
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            if (txtNIK.Text == "")
+            {
+                MessageBox.Show("NIK tidak boleh kosong!");
+                txtNIK.Focus();
+            } else if (txtNama.Text == "")
+            {
+                MessageBox.Show("Nama lengkap tidak boleh kosong");
+                txtNama.Focus();
+            } else if (txtTempat.Text == "")
+            {
+                MessageBox.Show("Tempat Lahir tidak boleh kosong!");
+                txtTempat.Focus();
+            } else if (rbLaki.Checked==false && rbPerempuan.Checked==false)
+            {
+                MessageBox.Show("Jenis Kelamin harus dipilih!");
+            } else if (txtAlamat.Text == "")
+            {
+                MessageBox.Show("Alamat tidak boleh kosong!");
+                txtAlamat.Focus();
+            } else if (txtTelepon.Text == "")
+            {
+                MessageBox.Show("Telepon tidak boleh kosong!");
+                txtTelepon.Focus();
+            } else if(cmbPekerjaan.SelectedIndex==-1)
+            {
+                MessageBox.Show("Pekerjaan harus dipilih!");
+            } else if (cmbStatus.SelectedIndex==-1)
+            {
+                MessageBox.Show("Status tidak boleh kosong!");
+            } else
+            {
+                rtbView.Clear();
+                rtbView.AppendText("NIK                     : " + txtNIK.Text + "\n");
+                rtbView.AppendText("Nama Lengkap            : " + txtNama.Text + "\n");
+                rtbView.AppendText("Tempat, Tanggal Lahir   : " + txtTempat.Text + " , " + dtpTanggal.Text + "\n");
+                rtbView.AppendText("Jenis Kelamin           : " + JenisKelamin + "\n");
+                rtbView.AppendText("Alamat                  : " + txtAlamat.Text + "\n");
+                rtbView.AppendText("No Telp                 : " + txtTelepon.Text + "\n");
+                rtbView.AppendText("Pekerjaan               : " + cmbPekerjaan.Text + "\n");
+                rtbView.AppendText("Status Perkawinan       : " + cmbStatus.SelectedItem + "\n");
+            }
+
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            rtbView.Clear();
+            txtNIK.Clear();
+            txtNama.Text = "";
+            txtTempat.Clear();
+            txtAlamat.Text = "";
+            txtTelepon.Text = "";
+            dtpTanggal.ResetText();
+            cmbPekerjaan.ResetText();
+            cmbStatus.ResetText();
+        
+        }
+
+        private void cmbStatus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNIK_TextChanged(object sender, EventArgs e)
+        {
+            string tString = txtNIK.Text;
+            if (tString.Trim() == "") return;
+
+            for(int i = 0; i<tString.Length; i++)
+            {
+                if(!char.IsNumber(tString[i]))
+                {
+                    MessageBox.Show("Silahkan inputkan angka saja!");
+                    txtNIK.Text = "";
+                    return;
+                }
+            }
+        }
+
+        private void txtTelepon_TextChanged(object sender, EventArgs e)
+        {
+            string telp = txtTelepon.Text;
+            if (telp.Trim() == "") return;
+
+            for (int i = 0; i < telp.Length; i++)
+            {
+                if (!char.IsNumber(telp[i]))
+                {
+                    MessageBox.Show("Silahkan inputkan angka saja!");
+                    txtTelepon.Text = "";
+                    return;
+                }
+
+                if (telp.Length > 12)
+                {
+                    MessageBox.Show("No Telp tidak boleh lebih dari 12 angka");
+                    txtTelepon.Text = "";
+                    return;
+                }
+            }
+        }
+    }
+}
